46dd7023234182925520e06775c79b73f45b9998
Cyril Dasari <cyril@Cyrils-MacBook-Air.local>
2017-12-15 16:08:51 -0500

    Initial

--- src/pages/topics/topics.ts
+++ src/pages/topics/topics.ts
@@ -5,7 +5,7 @@ import { NavController } from 'ionic-angular';
   selector: 'page-contact',
   templateUrl: 'contact.html'
 })
-export class ContactPage {
+export class TopicsPage {
 
   constructor(public navCtrl: NavController) {
 
